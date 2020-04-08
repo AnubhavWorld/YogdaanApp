7e7fe5f4932ecae58d651b0627ac4a8fc8b46999
Viren2737 <virensaroha123@gmail.com>
2020-04-08 12:04:48 +0530

    donate

--- app/src/main/java/com/example/yogdaan/donateActivity.java
+++ app/src/main/java/com/example/yogdaan/donateActivity.java
@@ -7,19 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button donateBtn;
+public class donateActivity extends AppCompatActivity {
+    private Button donateMoneyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_donate);
 
-        donateBtn=(Button)findViewById(R.id.donate_btn);
-        donateBtn.setOnClickListener(new View.OnClickListener() {
+        donateMoneyBtn=(Button)findViewById(R.id.donate_money_btn);
+
+        donateMoneyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,donateActivity.class);
+                Intent intent = new Intent(donateActivity.this,PaymentActivity.class);
                 startActivity(intent);
             }
         });
